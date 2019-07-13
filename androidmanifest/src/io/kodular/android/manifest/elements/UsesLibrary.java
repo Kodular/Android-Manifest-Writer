e02010b68fa3143000407ba5f423b69a0ec3c588
Sander Jochems <sanderjochems@hotmail.nl>
2019-07-13 20:52:02 +0200

    Create UsesLibrary.java

---
+++ androidmanifest/src/io/kodular/android/manifest/elements/UsesLibrary.java
@@ -0,0 +1,29 @@
+package io.kodular.android.manifest.elements;
+
+public class UsesLibrary extends BaseElement
+{
+    public UsesLibrary()
+    {
+        super("uses-feature");
+    }
+
+    public UsesLibrary(String library)
+    {
+        super("uses-library");
+
+        setName(library);
+    }
+
+    public UsesLibrary(String library, boolean required)
+    {
+        super("uses-library");
+
+        setName(library);
+        setRequired(required);
+    }
+
+    public void setRequired(boolean required)
+    {
+        properties.put("android:required", required ? "true" : "false");
+    }
+}
