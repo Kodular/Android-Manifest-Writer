50a2fe75cd1b4431fa5ba676285ca1ae338056fd
Sander Jochems <sanderjochems@hotmail.nl>
2019-07-13 20:51:47 +0200

    Create UsesPermission.java

---
+++ androidmanifest/src/io/kodular/android/manifest/elements/UsesPermission.java
@@ -0,0 +1,16 @@
+package io.kodular.android.manifest.elements;
+
+public class UsesPermission extends BaseElement
+{
+    public UsesPermission(String permission)
+    {
+        super("uses-permission");
+
+        setName(permission);
+    }
+
+    public void setMaxSdkVersion(String maxSdkVersion)
+    {
+        properties.put("android:maxSdkVersion", maxSdkVersion);
+    }
+}
