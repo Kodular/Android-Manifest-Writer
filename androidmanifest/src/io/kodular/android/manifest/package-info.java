345fa6e5f17893d0fd29352235220c8d10cbbaed
Pavitra <pavitragolchha@gmail.com>
2020-04-22 17:48:44 +0530

    Add package-info.java

--- androidmanifest/src/io/kodular/android/manifest/package-info.java
+++ androidmanifest/src/io/kodular/android/manifest/package-info.java
@@ -1,1 +1,15 @@
+@XmlSchema(
+        attributeFormDefault = XmlNsForm.QUALIFIED,
+        xmlns = {
+                @XmlNs(prefix = "android", namespaceURI = ANDROID_NS),
+                @XmlNs(prefix = "tools", namespaceURI = TOOLS_NS)
+        }
+)
 package io.kodular.android.manifest;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
+
+import static io.kodular.android.manifest.Constants.ANDROID_NS;
+import static io.kodular.android.manifest.Constants.TOOLS_NS;
